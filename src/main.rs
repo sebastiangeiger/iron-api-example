a0b0d09f9e91df64d328d3fe24e42a6d2fe3afee
Sebastian Geiger <sebastian.geiger@gmail.com>
2015-05-18 09:35:09 -0400

    Returning empty list by serializing Vec

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 extern crate iron;
 extern crate router;
+extern crate rustc_serialize;
 
 use iron::prelude::*;
 use iron::status;
 use router::Router;
+use rustc_serialize::json::{Json, ToJson};
 
 fn main() {
     let mut router = Router::new();
 
     fn items_path(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "[]")))
+        let result : Vec<String> = Vec::new();
+        let json_obj : Json = result.to_json();
+        let json_str : String = json_obj.to_string();
+        Ok(Response::with((status::Ok, json_str)))
     }
 
     router.get("/items", items_path);
