238d70c636180b72aed614a34cd24fe0b2f7331b
Sebastian Geiger <sebastian.geiger@gmail.com>
2015-05-22 23:31:59 -0400

    Testing JSON parsing in rust

--- src/main.rs
+++ src/main.rs
@@ -76,4 +76,21 @@ mod tests {
         };
         assert!(item != item_2);
     }
+
+    #[test]
+    fn test_item_parsing_with_valid_item() {
+        let item = Item {
+            name: "Bananas".to_string()
+        };
+        let json = "{\"name\":\"Bananas\"}";
+        let parsed_item : Item = json::decode(&json).unwrap();
+        assert_eq!(parsed_item, item);
+    }
+
+    #[test]
+    fn test_item_parsing_with_invalid_item() {
+        let json = "{\"garbage\":\"key\"}";
+        let parse_result : Result<Item,_> = json::decode(&json);
+        assert!(parse_result.is_err())
+    }
 }
